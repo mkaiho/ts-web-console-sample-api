fdbdfaaad550691ca430b24b3b5b6c3a449b420a
Masahiko Kaiho <mkaiho.cgengo@gmail.com>
2021-04-04 07:11:47 +0000

    Modify .js to .ts

--- src/server.ts
+++ src/server.ts
@@ -1,39 +1,16 @@
-#!/usr/bin/env node
+import app from './app'
+import debug from 'debug'
+import http from 'http'
 
-/**
- * Module dependencies.
- */
-
-var app = require('./app');
-var debug = require('debug')('ts-web-console-sample-api:server');
-var http = require('http');
-
-/**
- * Get port from environment and store in Express.
- */
-
-var port = normalizePort(process.env.PORT || '3001');
+const port = normalizePort(process.env.PORT || '3001');
 app.set('port', port);
 
-/**
- * Create HTTP server.
- */
-
-var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
+const server = http.createServer(app);
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
 
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
+function normalizePort(val: string) {
   var port = parseInt(val, 10);
 
   if (isNaN(port)) {
@@ -53,7 +30,7 @@ function normalizePort(val) {
  * Event listener for HTTP server "error" event.
  */
 
-function onError(error) {
+function onError(error: NodeJS.ErrnoException) {
   if (error.syscall !== 'listen') {
     throw error;
   }
@@ -85,6 +62,6 @@ function onListening() {
   var addr = server.address();
   var bind = typeof addr === 'string'
     ? 'pipe ' + addr
-    : 'port ' + addr.port;
+    : 'port ' + addr!.port;
   debug('Listening on ' + bind);
 }
